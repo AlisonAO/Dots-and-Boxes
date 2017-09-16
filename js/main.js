c5df37562a873f5b88b0afebc713dbefc51c650e
AlisonAO <alison_opeloyeru@hotmail.co.uk>
2017-09-16 23:43:18 +0100

    Created border classes

--- js/main.js
+++ js/main.js
@@ -9,30 +9,22 @@ $(function (){
 
 	function playGame() {
 		$('.lines').one('click', function(event) {
-			
 			count++;
 		// checkWinner();
-		
-
 			if (currentPlayer === 1) {
 				$(this).addClass('player1 line-drawn');
 				currentPlayer = 0; 
 				checkBox();
-				
-					
 			} else {
 				$(this).addClass('player2 line-drawn');
 				currentPlayer = 1;
-				checkBox();
-				
+				checkBox();	
 			};
 			
 		});
 	};
 
 	// All lines and boxes as variables
-
-	var $line1 = $("#1of0");
 
 	function checkBox() {
 		for (var i = 0; i < maxMoves; i++) {
