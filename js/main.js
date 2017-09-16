a14ba1b884f802df16f68b3f2d2484dbac273533
AlisonAO <alison_opeloyeru@hotmail.co.uk>
2017-09-17 00:33:09 +0100

    Added ids to some of the elements

--- js/main.js
+++ js/main.js
@@ -26,12 +26,20 @@ $(function (){
 
 	// All lines and boxes as variables
 
+//<---This may need fixing--->
 	function checkBox() {
 		for (var i = 0; i < maxMoves; i++) {
-			if ($("#1of0").hasClass('line-drawn')) {
-			 console.log('working');
-			 $("#1of0").removeClass('line-drawn');	
-			 maxMoves--;
+			if ($('#1of1').hasClass('line-drawn') && currentPlayer === 0) {
+				console.log('working');
+				$('.box1').addClass('player1-bordertop');
+				$('#1of1').removeClass('line-drawn');	
+				maxMoves--;
+			}
+			if ($('#1of1').hasClass('line-drawn') && currentPlayer === 1) {
+				console.log('working');
+				$('.box1').addClass('player2-bordertop');
+				$('#1of1').removeClass('line-drawn');	
+				maxMoves--;
 			}
 		}
 	}
