3e5699f5e9614a7258e2b345e848830bc1c390f0
AlisonAO <alison_opeloyeru@hotmail.co.uk>
2017-09-17 14:28:59 +0100

    First test of the checkbox function worked

--- js/main.js
+++ js/main.js
@@ -13,33 +13,28 @@ $(function (){
 		// checkWinner();
 			if (currentPlayer === 1) {
 				$(this).addClass('player1 line-drawn');
-				currentPlayer = 0; 
 				checkBox();
+				currentPlayer = 0; 
+				
 			} else {
 				$(this).addClass('player2 line-drawn');
-				currentPlayer = 1;
 				checkBox();	
+				currentPlayer = 1;
+				
 			};
 			
 		});
 	};
 
-	// All lines and boxes as variables
 
 //<---This may need fixing--->
 	function checkBox() {
 		for (var i = 0; i < maxMoves; i++) {
-			if ($('#1of1').hasClass('line-drawn') && currentPlayer === 0) {
+			if ($('#1of1').hasClass('line-drawn')) {
 				console.log('working');
-				$('.box1').addClass('player1-bordertop');
 				$('#1of1').removeClass('line-drawn');	
 				maxMoves--;
-			}
-			if ($('#1of1').hasClass('line-drawn') && currentPlayer === 1) {
-				console.log('working');
-				$('.box1').addClass('player2-bordertop');
-				$('#1of1').removeClass('line-drawn');	
-				maxMoves--;
+				$('#box1').addClass('bordertop');
 			}
 		}
 	}
